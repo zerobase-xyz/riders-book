9bc5e5e0bb13a143ebe106ea4d26bed8de93afaf
zerobase-xyz <offroadrider.mx.0723@gmail.com>
2019-05-07 00:10:48 +0900

    add healthcheck

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Apistatus from './views/Apistatus.vue'
 
 Vue.use(Router)
 
@@ -15,9 +15,9 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About
+      path: '/apistatus',
+      name: 'apistatus',
+      component: Apistatus
     }
   ]
 })
---
+++ src/views/Apistatus.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ apiStatus }}</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import axios from 'axios'
+@Component({})
+export default class HelloWorld extends Vue {
+  apiStatus: string = 'now loading ...'
+  mounted(): void {
+    axios
+      .get('https://api.riders-book.com/healthcheck')
+      .then(response => (this.apiStatus = response.data.status))
+      .catch(err => (this.apiStatus = err))
+  }
+}
+</script>
